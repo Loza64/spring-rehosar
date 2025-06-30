5eb8c64818cb07ecd1eda0c7e37fb1e1f7d6e209
Roberto Loza <robertoloxa88@gmail.com>
2025-06-30 12:39:57 -0600

    fix cors

--- src/main/java/com/pnc/project/config/WebConfig.java
+++ src/main/java/com/pnc/project/config/WebConfig.java
@@ -10,31 +10,24 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
     @Bean
-    public HttpMessageConverter<Object> CustomJsonConverter() {
+    public HttpMessageConverter<Object> customJsonConverter() {
         return new MappingJackson2HttpMessageConverter();
     }
 
     @Bean
-    public PasswordEncoder Encoder() {
+    public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://rehosar.vercel.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("*")
+                .maxAge(3600);
     }
-
 }
-
